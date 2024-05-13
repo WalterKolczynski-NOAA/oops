3600b6b855097ec19043ab05da8fa514cee18930
Mark A Potts <mark.potts@noaa.gov>
2024-05-13 17:55:06 -0400

    Seems to be working

--- src/oops/base/StateSet.h
+++ src/oops/base/StateSet.h
@@ -183,8 +183,12 @@ std::unique_ptr<StateSet<MODEL> > StateSet<MODEL>::get_local(const eckit::mpi::C
 
   local = std::unique_ptr<StateSet<MODEL> >(new StateSet(subgeom, this->variables(), this->times(), 
                   this->commTime(), local_ens, oops::mpi::myself()));
-  std::vector<int> buf(2);
+  std::vector<int> buf(3);
+  std::vector<int> recipients; // This will contain list of mpi tasks where local tile should be sent
+  std::vector<int> senders; // This will contain list of mpi tasks which will be sending data to me
+  std::vector<int> tileEnsNum; // This will contain list of ensemble numbers that I am receiving
   std::vector<int> global_indices;
+  int mytile = this->geometry().tileNum();
   this->geometry().get_indices(global_indices); // returns ist, iend, jst, jend, kst, kend, npz
   int nxg = global_indices[1] - global_indices[0] + 1;
   int nyg = global_indices[3] - global_indices[2] + 1;
@@ -195,11 +199,21 @@ std::unique_ptr<StateSet<MODEL> > StateSet<MODEL>::get_local(const eckit::mpi::C
   StateSet<MODEL> tmpState = StateSet<MODEL>(*this);  //ths tmpState is full size
   for(int i = 0; i < global.size(); ++i){
     if(i == mytask ) { //mytask is global rank
-      buf[0] = this->geometry().tileNum();  // The tile number about to be sent
-      buf[1] = ensNum;
+      buf[0] = mytile;  // The tile number that this rank holds
+      buf[1] = subgeom.tileNum();  // The tile number that I need
+      buf[2] = ensNum;  // the ensemble number this tile belongs to
     }
     std::cout << "HEY, starting broadcasts " << i << " on task " << mytask << std::endl;
     global.broadcast(buf, i);
+    std::cout << "buf on task " << i << " is " << buf[0] << " " << buf[1] << std::endl;
+    if(buf[0] == subgeom.tileNum()) { //  if the tile that the sender has matches what I need, this is one of my senders
+      senders.push_back(i);
+      tileEnsNum.push_back(buf[2]);
+    }
+    if(buf[1] == mytile) { //  if the subgeometry tile needed matches the tile I have, this is who I will send it to
+      recipients.push_back(i);
+    }
+#if 0
     //broadcast the state
     const std::vector<util::DateTime> times = (*this).validTimes(); 
     std::cout << "before serialize, times are " << times[0] << " and " << times.size() << std::endl;
@@ -233,9 +247,94 @@ std::unique_ptr<StateSet<MODEL> > StateSet<MODEL>::get_local(const eckit::mpi::C
       std::cout << "dont have this tile on " << mytask << " " << std::endl;
     }
   std::cout << "writing out tmpState[0]" << tmpState[0] << std::endl;
+#endif
   }
-  std::cout << "DONE deserializing subgeom state 0 is " << (*local)(0,0) << std::endl;
-  std::cout << "DONE deserializing subgeom state 1 is " << (*local)(0,1) << std::endl;
+  std::cout << "From task " << mytask << " my recipients are " << recipients[0] << " " << recipients[1] << "|||" << recipients.size() << std::endl;
+  std::cout << "From task " << mytask << " my senders are " << senders[0] << " " << senders[1] << "|||" << senders.size() << std::endl;
+  std::vector<eckit::mpi::Request> send_req_;
+  std::vector<eckit::mpi::Request> recv_req_;
+  std::vector<size_t> recv_tasks_;
+  size_t indx = 0;
+
+  (*this)(0,0).serialize(zz);  //serialize the state in time 0 and local_ens_number 0
+  std::vector<std::vector<double> > zz_recv(senders.size());  // vector of vectors to receive each buffer
+  for( int j = 0; j < recipients.size(); ++j ) {  // reserve space for receiving
+     for( int k = 0; k < zz.size(); ++k ) {
+       zz_recv[j].push_back(0.0);
+     }
+  }
+  std::cout << "From task " << mytask << " zz size is " << zz.size() << " and  " << zz_recv[0].size() << "|||" << zz_recv[1].size() << std::endl;
+  for( int j = 0; j < recipients.size(); ++j ) {  // loop through list of recipients/senders and send/recv
+    if(recipients[j] != mytask) {  // dont send anything to myself
+      std::cout << "on mytask " << mytask << " sending to " << recipients[j] << std::endl;
+      send_req_.push_back(global.iSend(&zz.front(), zz.size(),recipients[j],ensNum));
+
+    }
+    if(senders[j] != mytask) { // dont need to receive from myself
+        std::cout << "on mytask " << mytask << " looking for mesg from " << senders[j] << std::endl;
+        recv_req_.push_back(global.iReceive(&zz_recv[j][0], zz.size(),senders[j],tileEnsNum[j]));
+        recv_tasks_.push_back(tileEnsNum[j]);
+        std::cout << "on task " << mytask << " pushing back recv_tasks " << tileEnsNum[j] << std::endl;
+    } else { 
+      // copy from my local version
+      size_t itask = ensNum-1;
+      zz_recv[itask] = zz;
+      indx = 0;
+      std::cout << "size of zz_recv[" << itask << "] is " << zz_recv[itask].size() << std::endl;
+      tmpState[0].deserialize(zz_recv[itask],indx); // put the full zz recv vector in a state
+      int size_fld = (*local)(0,0).serialSize() - 3;  // get the serialsize of the smaller tile (local)
+      std::vector<double> yy;
+      int ist, iend, jst, jend, npz;
+      std::vector<int> indices;
+      subgeom.get_indices(indices);
+      ist = indices[0];
+      iend = indices[1];
+      jst = indices[2];
+      jend = indices[3];
+      std::cout << "calling ssect from " << mytask << " " << ist << "," << iend << "," << jst << "," << jend << std::endl;
+      tmpState[0].serializeSect(yy,ist,iend,jst,jend,size_fld);  // copy out on the part of the state we need
+      Log::info() << "Start of yy is " << yy[0] << " " << yy[10] << std::endl;
+      std::cout << "returned fromssect from " << mytask << " " << yy.size() << std::endl;
+      std::cout << "serializing subgeom tileNum " << subgeom.tileNum() << "," << ensNum << std::endl;
+      std::cout << "Start of yy is " << yy[0] << "," << yy[10] << std::endl;
+      indx = 0;
+      (*local)(0,itask).deserialize(yy,indx); // assuming itask is the tag number, put this state in ens number itask
+      std::cout << "DONE deserializing subgeom tileNum " << subgeom.tileNum() << std::endl;
+    } 
+  }
+  std::cout << "starting to look for messages. I need " << recv_req_.size() << std::endl;
+//  tmpState[0].deserialize(zz,indx);  
+  for( size_t r = 0; r < recv_req_.size(); ++r) {
+    int ireq = -1;
+    eckit::mpi::Status rst = global.waitAny(recv_req_, ireq);
+    std::cout << "On rank " << mytask << " got a message with ireq = " << ireq << " " << recv_tasks_[ireq] << std::endl;
+    ASSERT(rst.error() == 0);
+    size_t itask = recv_tasks_[ireq] - 1;
+      indx = 0;
+      std::cout << "size of zz_recv[" << itask << "] is " << zz_recv[itask].size() << std::endl;
+      tmpState[0].deserialize(zz_recv[itask],indx); // put the full zz recv vector in a state
+      int size_fld = (*local)(0,0).serialSize() - 3;  // get the serialsize of the smaller tile (local)
+      std::vector<double> yy;
+      int ist, iend, jst, jend, npz;
+      std::vector<int> indices;
+      subgeom.get_indices(indices);
+      ist = indices[0];
+      iend = indices[1];
+      jst = indices[2];
+      jend = indices[3];
+      std::cout << "calling ssect from " << mytask << " " << ist << "," << iend << "," << jst << "," << jend << std::endl;
+      tmpState[0].serializeSect(yy,ist,iend,jst,jend,size_fld);  // copy out on the part of the state we need
+      Log::info() << "Start of yy is " << yy[0] << " " << yy[10] << std::endl;
+      std::cout << "returned fromssect from " << mytask << " " << yy.size() << std::endl;
+      std::cout << "serializing subgeom tileNum " << subgeom.tileNum() << "," << ensNum << std::endl;
+      std::cout << "Start of yy is " << yy[0] << "," << yy[10] << std::endl;
+      indx = 0;
+      (*local)(0,itask).deserialize(yy,indx); // assuming itask is the tag number, put this state in ens number itask
+      std::cout << "DONE deserializing subgeom tileNum " << subgeom.tileNum() << std::endl;
+
+  }
+//  std::cout << "DONE deserializing subgeom state 0 is " << (*local)(0,0) << std::endl;
+//  std::cout << "DONE deserializing subgeom state 1 is " << (*local)(0,1) << std::endl;
   const std::vector<util::DateTime> times = (*local).validTimes(); 
   return(std::move(local));
 }
